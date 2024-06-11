0f886f409d51c962bbfe16b11428a1899fdbbed7
OdilonDAlex <odilondalex2600@gmail.com>
2024-06-11 09:05:23 -0100

    Implémentation des relations entre post et user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
         'content',
     ];
 
 
-    use HasFactory;
+    public function users(): BelongsTo{
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
