7e3cb7572c7abe886c566d8d2215ac19fce86025
OdilonDAlex <odilondalex2600@gmail.com>
2024-06-18 11:53:25 +0300

    Creation du model EventModel et implémentation du relation 'author' entre User et EventModel

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class EventModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'at'
+    ];
+
+    public function author(): BelongsTo{
+        return $this->belongsTo(User::class, 'author_id');
+    } 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -58,5 +59,9 @@ class User extends Authenticatable
 
     public function getFullName(): string {
         return $this->name . " " . $this->firstname; 
+    }
+
+    public function event_models(): HasMany {
+        return $this->hasMany(EventModel::class, 'author_id');
     }
 }
